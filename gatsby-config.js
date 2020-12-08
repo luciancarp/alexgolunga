1d926824a527b6c3dad3cd667a57089bd04e02ce
Lucian Carp <lucian.g.carp@gmail.com>
2020-12-08 18:54:50 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "BJITWGXXn1aC_VWjqpTn0WwTKzrp48sK326ry88MPps",
+        spaceId: "",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+  ],
+};
