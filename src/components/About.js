df0a3ddc1f482ae61d5f7e6e27fde148b069b33d
Lucian Carp <lucian.g.carp@gmail.com>
2020-12-11 18:36:16 +0200

    Create About component

---
+++ src/components/About.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import { spaces } from '../style/global'
+
+const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulAbout {
+        edges {
+          node {
+            title
+            description
+          }
+        }
+      }
+    }
+  `)
+
+  const { title, description } = data.allContentfulAbout.edges[0].node
+
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  margin-bottom: 50vh;
+`
+
+const Title = styled.h1`
+  margin-bottom: ${spaces.wide};
+`
+
+const Description = styled.h3``
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import * as React from 'react'
 
 import Layout from '../components/Layout'
 import Projects from '../components/Projects'
+import About from '../components/About'
 
 // markup
 const IndexPage = () => {
   return (
     <Layout>
+      <About />
       <Projects />
     </Layout>
   )
