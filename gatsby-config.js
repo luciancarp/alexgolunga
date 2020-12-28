d5b6087a7f902376c920e255461b3fdf1faf736c
Lucian Carp <lucian.g.carp@gmail.com>
2020-12-28 17:58:56 +0200

    Add gatsby-plugin-webpack-bundle-analyser-v2 plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,11 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyser-v2',
+      options: {
+        devMode: true,
+      },
+    },
   ],
 }
