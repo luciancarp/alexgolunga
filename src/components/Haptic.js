aa2a70d5a2be30a7d8e856952d18c0ba03e537c0
Lucian Carp <lucian.g.carp@gmail.com>
2020-12-29 18:53:45 +0200

    Add content in Haptic

--- src/components/Haptic.js
+++ src/components/Haptic.js
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react'
 import styled from 'styled-components'
+import { graphql, useStaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
 
 import { Opacity, Translate } from './Animations'
 import { useHasBeenPartlyVisible } from '../hooks/useVisibility'
@@ -7,10 +9,36 @@ import { spaces } from '../style/global'
 
 const id = 'haptic'
 const title = 'Haptic Design'
-const content =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam efficitur, leo vel porta dapibus, lorem tortor placerat nulla, at gravida ligula lorem auctor lorem. Nulla maximus condimentum ornare. Duis iaculis nibh nec nisi commodo, vel tincidunt mi venenatis. Cras at velit pretium, finibus augue vel, consequat mi. Sed cursus neque id aliquet hendrerit. Vivamus massa metus, pulvinar eget turpis at, dictum placerat libero. Suspendisse potenti. Cras eu tincidunt nunc. Ut quis orci nibh. Nam el'
+
+export const Image = graphql`
+  fragment Image on File {
+    childImageSharp {
+      fluid(maxWidth: 600, quality: 90) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+`
 
 const Haptic = () => {
+  const query = useStaticQuery(graphql`
+    query {
+      weaponLaunchImpact: file(
+        relativePath: { eq: "Weapon_Launch_Impact.png" }
+      ) {
+        ...Image
+      }
+      particlesHaptic: file(relativePath: { eq: "ParticlesHaptic.png" }) {
+        ...Image
+      }
+      superhotReworkHaptic: file(
+        relativePath: { eq: "SuperhotReworkHaptic.png" }
+      ) {
+        ...Image
+      }
+    }
+  `)
+
   const halfPage = useRef()
   const hasScrolled = useHasBeenPartlyVisible(halfPage, 0.1)
   return (
@@ -19,7 +47,32 @@ const Haptic = () => {
         <Opacity>
           <Translate>
             <Title>{title}</Title>
-            <p>{content}</p>
+            <p>
+              If you have an iPhone with iOS 13.0 or later, you can download the
+              Lofelt Studio app and scan these QR codes to experience the
+              haptics I designed for a few sounds. Make sure your phone is not
+              in Silent Mode in order to hear the audio.
+            </p>
+            <GridContainer>
+              <GridItem col={'1'} row={1}>
+                <StyledImg
+                  fluid={query.superhotReworkHaptic.childImageSharp.fluid}
+                />
+                <p>Section from SUPERHOT: MCD trailer Re-Sound Design</p>
+              </GridItem>
+              <GridItem col={'2'} row={1}>
+                <StyledImg
+                  fluid={query.particlesHaptic.childImageSharp.fluid}
+                />
+                <p>Jitter Particles Animation</p>
+              </GridItem>
+              <GridItem col={'3'} row={1}>
+                <StyledImg
+                  fluid={query.weaponLaunchImpact.childImageSharp.fluid}
+                />
+                <p>Weapon Launch / Impact</p>
+              </GridItem>
+            </GridContainer>
           </Translate>
         </Opacity>
       ) : (
@@ -28,6 +81,28 @@ const Haptic = () => {
     </Container>
   )
 }
+
+const StyledImg = styled(Img)`
+  margin-bottom: ${spaces.narrow};
+  border-style: solid;
+  border-color: ${(props) => props.theme.text};
+  border-width: 2px;
+`
+
+const GridItem = styled.div`
+  grid-column: ${(props) => (props.col ? props.col : '1 / 2')};
+  grid-row: ${(props) => (props.row ? `${props.row}` : '1')};
+`
+
+const GridContainer = styled.div`
+  margin-top: ${spaces.wide};
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: ${spaces.wide};
+  row-gap: ${spaces.wide};
+  grid-auto-rows: minmax(100px, auto);
+`
 
 const Placeholder = styled.div`
   height: 100vh;
