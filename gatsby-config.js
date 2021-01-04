662166b15756fa1c51d558d6b90ab81de141e39c
Lucian Carp <lucian.g.carp@gmail.com>
2021-01-04 19:28:56 +0200

    Update gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,16 +47,5 @@ module.exports = {
         icon: `src/assets/images/alex-golunga.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      },
-    },
   ],
 }
