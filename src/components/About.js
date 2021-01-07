ead63ed7c32b963276162d044f0f1fdec810d9b7
Lucian Carp <lucian.g.carp@gmail.com>
2021-01-07 19:14:16 +0200

    Remove backdrop-filter in About

--- src/components/About.js
+++ src/components/About.js
@@ -142,8 +142,8 @@ const About = () => {
               key={key}
               background={
                 theme.name === 'dark'
-                  ? 'rgba(0, 0, 0, 0.5)'
-                  : 'rgba(238, 238, 238, 0.5)'
+                  ? 'rgba(0, 0, 0, 1)'
+                  : 'rgba(238, 238, 238, 1)'
               }
               shadow={
                 theme.name === 'dark'
@@ -246,7 +246,7 @@ const Placeholder = styled.div`
 const Container = styled('div').attrs(
   ({
     state,
-    duration = 'height 0.2s, padding-top 0.2s, position 0.2s, background-color 0.2s, backdropFilter 0.2s,WebkitBackdropFilter 0.2s, box-shadow 0.2s',
+    duration = 'height 0.2s, padding-top 0.2s, position 0.2s, background-color 0.2s, box-shadow 0.2s',
     start = '20vh',
     end = '6rem',
     background = 'rgba(0, 0, 0, 0.5)',
@@ -257,8 +257,6 @@ const Container = styled('div').attrs(
       height: `${!state ? start : end}`,
       paddingTop: `${!state ? '5vh' : '0.50rem'}`,
       backgroundColor: `${!state ? 'rgba(0, 0, 0, 0)' : background}`,
-      backdropFilter: `${!state ? 'blur(0)' : `blur(10px)`}`,
-      WebkitBackdropFilter: `${!state ? 'blur(0)' : `blur(10px)`}`,
       boxShadow: `${!state ? '0px 0px 0px 0px rgba(0, 0, 0, 0)' : shadow}`,
     },
   })
@@ -403,10 +401,10 @@ const MInfoStickyContainer = styled.div`
 
   background-color: ${(props) => props.theme.background};
 
-  @supports (backdrop-filter: none) {
+  /* @supports (backdrop-filter: none) {
     background-color: ${(props) => props.theme.backgroundTransp};
     backdrop-filter: blur(${(props) => props.theme.blur});
-  }
+  } */
 
   /* box-shadow: 0px 0px 10px 5px ${(props) => props.theme.backgroundTransp}; */
 
